29ca64c4cb02445642e62b0c244f6c4c1eae4381
Arseniy Lartsev <arseniy.lartsev@qodes.se>
2024-08-22 11:24:27 +0200

    Initial OCPP 2.0.1 support

---
+++ custom_components/ocpp/ocppv201.py
@@ -0,0 +1,121 @@
+"""Representation of a OCPP 2.0.1 charging station."""
+
+from datetime import datetime
+import logging
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+import websockets.server
+
+from ocpp.routing import on
+from ocpp.v201 import call, call_result
+
+from .chargepoint import CentralSystemSettings, OcppVersion
+from .chargepoint import ChargePoint as cp
+
+from .const import (
+    DOMAIN,
+)
+
+_LOGGER: logging.Logger = logging.getLogger(__package__)
+logging.getLogger(DOMAIN).setLevel(logging.INFO)
+
+
+class ChargePoint(cp):
+    """Server side representation of a charger."""
+
+    def __init__(
+        self,
+        id: str,
+        connection: websockets.server.WebSocketServerProtocol,
+        hass: HomeAssistant,
+        entry: ConfigEntry,
+        central: CentralSystemSettings,
+        interval_meter_metrics: int = 10,
+        skip_schema_validation: bool = False,
+    ):
+        """Instantiate a ChargePoint."""
+
+        super().__init__(
+            id,
+            connection,
+            OcppVersion.V201,
+            hass,
+            entry,
+            central,
+            interval_meter_metrics,
+            skip_schema_validation,
+        )
+
+    async def async_update_device_info_v201(self, boot_info: dict):
+        """Update device info asynchronuously."""
+
+        _LOGGER.debug("Updating device info %s: %s", self.central.cpid, boot_info)
+        await self.async_update_device_info(
+            boot_info.get("serial_number", None),
+            boot_info.get("vendor_name", None),
+            boot_info.get("model", None),
+            boot_info.get("firmware_version", None),
+        )
+
+    @on("BootNotification")
+    def on_boot_notification(self, charging_station, reason, **kwargs):
+        """Perform OCPP callback."""
+        resp = call_result.BootNotification(
+            current_time=datetime.utcnow().isoformat() + "Z",
+            interval=10,
+            status="Accepted",
+        )
+
+        self.hass.async_create_task(
+            self.async_update_device_info_v201(charging_station)
+        )
+        self.register_boot_notification()
+        return resp
+
+    @on("Heartbeat")
+    def on_heartbeat(self, **kwargs):
+        """Perform OCPP callback."""
+        return call_result.Heartbeat(
+            current_time=datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S") + "Z"
+        )
+
+    @on("StatusNotification")
+    @on("FirmwareStatusNotification")
+    @on("MeterValues")
+    @on("LogStatusNotification")
+    @on("NotifyEvent")
+    def ack(self, **kwargs):
+        """Perform OCPP callback."""
+        return call_result.StatusNotification()
+
+    @on("Authorize")
+    def on_authorize(self, idToken, **kwargs):
+        """Perform OCPP callback."""
+        return call_result.Authorize(id_token_info={"status": "Accepted"})
+
+    @on("TransactionEvent")
+    def on_transaction_event(
+        self, event_type, timestamp, trigger_reason, seq_no, transaction_info, **kwargs
+    ):
+        """Perform OCPP callback."""
+        return call_result.TransactionEvent(id_token_info={"status": "Accepted"})
+
+    @on("SignCertificate")
+    def on_sign_certificate(self, csr, **kwargs):
+        """Perform OCPP callback."""
+        return call_result.SignCertificate(status="Accepted")
+
+    @on("Get15118EVCertificate")
+    def on_get_v2g_certificate(
+        self, iso15118_schema_version, action, exi_request, **kwargs
+    ):
+        """Perform OCPP callback."""
+        return call_result.Get15118EVCertificate(
+            status="Failed",
+            exi_response="",
+            status_info={
+                "reasonCode": "Unspecified",
+                "additionalInfo": "Not implemented",
+            },
+        )
